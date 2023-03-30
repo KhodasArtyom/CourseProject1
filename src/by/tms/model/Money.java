bd046d0f359740f5a7d37bb86d14914dbbdb386b
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-03-30 20:32:38 +0300

    add model

--- src/by/tms/model/Money.java
+++ src/by/tms/model/Money.java
@@ -1,6 +1,36 @@
 package by.tms.model;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 public class Money {
 
+    private final Currency currency;
 
+    private final BigDecimal sellPrice;
+
+    private final BigDecimal buyPrice;
+
+    public Money(Currency currency, BigDecimal sellPrice, BigDecimal buyPrice) {
+        if ((currency==null) || (sellPrice.compareTo(BigDecimal.ZERO) <=0)
+                || (buyPrice.compareTo(BigDecimal.ZERO)<=0)) {
+            throw new IllegalArgumentException();
+
+        }
+        this.currency = currency;
+        this.sellPrice = sellPrice;
+        this.buyPrice = buyPrice;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getSellPrice() {
+        return sellPrice;
+    }
+
+    public BigDecimal getBuyPrice() {
+        return buyPrice;
+    }
 }
