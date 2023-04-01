5912eb07b775495ba34f5b3cf7829d028689083d
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-04-01 11:36:43 +0300

    redact Properties with one more variable

--- src/by/tms/config/RepositoryProperties.java
+++ src/by/tms/config/RepositoryProperties.java
@@ -1,19 +1,26 @@
 package by.tms.config;
 
 import java.nio.file.Path;
+import java.util.Currency;
 
 public class RepositoryProperties {
 
     private final Path fileDirectory;
+    private final Currency isoCode;
 
-    public RepositoryProperties(Path fileDirectory) {
+    public RepositoryProperties(Path fileDirectory,Currency isoCode) {
         this.fileDirectory = fileDirectory;
-        if(fileDirectory==null) {
+        this.isoCode = isoCode;
+        if(fileDirectory==null || isoCode == null) {
             throw new NullPointerException();
         }
-    }
 
+    }
     public Path getFileDirectory() {
         return fileDirectory;
+    }
+
+    public Currency getIsoCode() {
+        return isoCode;
     }
 }
---
+++ src/by/tms/repository/MoneyReposService.java
@@ -0,0 +1,10 @@
+package by.tms.repository;
+
+import by.tms.model.Money;
+
+import java.util.ArrayList;
+
+public interface MoneyReposService {
+
+    ArrayList<Money> getAllRates();
+}
